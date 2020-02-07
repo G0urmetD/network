b60e93ff85cb1a7749de3aa730015b0e62e65534
G0urmetD <60777262+G0urmetD@users.noreply.github.com>
2020-02-07 11:20:17 +0100

    Update and rename network.sh to openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt update
+apt install openvpn -y
+
 echo "source /etc/network/interfaces.d/*" > /etc/network/interfaces
 echo "" >> /etc/network/interfaces
 echo "auto enp0s3" >> /etc/network/interfaces
@@ -7,3 +10,5 @@ echo "iface enp0s3 inet static" >> /etc/network/interfaces
 echo "address 10.16.203.223" >> /etc/network/interfaces
 echo "netmask 255.0.0.0" >> /etc/network/interfaces
 echo "gateway 10.16.1.245" >> /etc/network/interfaces
+
+systemctl restart networking
