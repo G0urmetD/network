fcbbe7a5730c82624712b8ef7ff46ffc2a2d37c3
G0urmetD <60777262+G0urmetD@users.noreply.github.com>
2020-02-07 13:23:34 +0100

    Update network.sh

--- network.sh
+++ network.sh
@@ -2,14 +2,17 @@
 
 apt update
 
-echo "source /etc/network/interfaces.d/*" > /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-echo "auto enp0s3" >> /etc/network/interfaces
-echo "iface enp0s3 inet static" >> /etc/network/interfaces
-echo "address 10.16.203.223" >> /etc/network/interfaces
-echo "netmask 255.0.0.0" >> /etc/network/interfaces
-echo "gateway 10.16.1.245" >> /etc/network/interfaces
+path="/etc/network/interfaces"
 
-echo "nameserver 10.16.1.253" > /etc/resolv.conf
+echo "source /etc/network/interfacesces.d/*" > $path
+echo "" >> $path
+echo "auto enp0s3" >> $path
+echo "iface enp0s3 inet static" >> $path
+echo "address 10.16.203.223" >> $path
+echo "netmask 255.0.0.0" >> $path
+echo "gateway 10.16.1.245" >> $path
+
+path2="/etc/resolv.conf"
+echo "nameserver 10.16.1.253" > $path2
 
 systemctl restart networking
