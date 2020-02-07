23e416b3c4f437d10efc906263321bacbf9d7386
G0urmetD <60777262+G0urmetD@users.noreply.github.com>
2020-02-07 14:50:44 +0100

    Create openvpn-client.sh

---
+++ openvpn-client.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "client" > /etc/hostname
+
+apt update
+
+rm -r server.crt
+rm -r server.key
+rm -r server.conf
+rm -r dh.pem
+rm -r server
+
+cp /root/my_ca/pki/issued/client01.crt
+cp /root/my_ca/pki/private/client01.key
+
+clientpath="/etc/openvpn/client.conf"
+
+echo "remote 10.16.203.223 1194" > $clientpath
+echo "proto udp" >> $clientpath
+echo "dev tun" >> $clientpath
+echo "ca ca.crt" >> $clientpath
+echo "cert client01.crt" >> $clientpath
+echo "key client01.key" >> $clientpath
+echo "dh dh.pem" >> $clientpath
+echo "ping-timer-rem" >> $clientpath
+echo "keepalive 20 180" >> $clientpath
