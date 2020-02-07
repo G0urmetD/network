61777326f0b18644351d7cb1fa12f32f9c9e2daa
G0urmetD <60777262+G0urmetD@users.noreply.github.com>
2020-02-07 11:11:30 +0100

    Create network.sh

---
+++ network.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "source /etc/network/interfaces.d/*" > /etc/network/interfaces
+echo "" >> /etc/network/interfaces
+echo "auto enp0s3" >> /etc/network/interfaces
+echo "iface enp0s3 inet static" >> /etc/network/interfaces
+echo "address 10.16.203.223" >> /etc/network/interfaces
+echo "netmask 255.0.0.0" >> /etc/network/interfaces
+echo "gateway 10.16.1.245" >> /etc/network/interfaces
