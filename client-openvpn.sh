70ac0491248d4d717feca8c0dbb93c400c2ef1a5
G0urmetD <60777262+G0urmetD@users.noreply.github.com>
2020-03-03 07:44:41 +0100

    Update client-openvpn.sh

--- client-openvpn.sh
+++ client-openvpn.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+# set the hostname to "client"
 echo "client" > /etc/hostname
 
+# make an update
 apt update
 
+# go into the path and remove all not needed server documents
 cd /etc/openvpn/ 
 rm -r server.crt
 rm -r server.key
@@ -11,11 +13,14 @@ rm -r server.conf
 rm -r dh.pem
 rm -r server
 
+# copy the client KEY and CERT into the openvpn path
 cp /root/my_ca/pki/issued/client01.crt /etc/openvpn/
 cp /root/my_ca/pki/private/client01.key /etc/openvpn/
 
+# path of the client.conf
 clientpath="/etc/openvpn/client.conf"
 
+# asking for the IP
 echo Server-IP?
 read Sip
 
@@ -29,4 +34,5 @@ echo "dh dh.pem" >> $clientpath
 echo "ping-timer-rem" >> $clientpath
 echo "keepalive 20 180" >> $clientpath
 
+# restart the opvenpn service
 systemctl restart openvpn
